c142520c77af93d45683c50f565659df8b9d3e32
Timofey Tyrin <timat-ti@mail.ru>
2017-03-16 23:09:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,56 +2,51 @@
 
 int main()
 {
+
+    TBinaryTree tree;
+
+
+    try
     {
-        TBinaryTree tree;
+        tree.Insert(10);
+        tree.Insert(20);
+        tree.Insert(1);
+        tree.Insert(2);
+        tree.Insert(15);
 
+        tree.Insert(-10);
+        tree.Insert(10);
+    }
+    catch(const TExistElementException & e)
+    {
+        std::cout << e.what();
+    }
 
-        try
-        {
-            tree.Insert(10);
-            tree.Insert(20);
-            tree.Insert(1);
-            tree.Insert(2);
-            tree.Insert(15);
+    std::cout << tree << std::endl << std::endl;
 
-            tree.Insert(-10);
-            tree.Insert(10);
-        }
-        catch(const TExistElementException & e)
-        {
-            std::cout << e.what();
-        }
+    try
+    {
+        TBinaryTree::TNode * found = tree.Find(2);
+        if(found)
+            std::cout << found->Data;
+    }
+    catch (const TNotFoundException & e)
+    {
+        std::cout << "Exception works" << std::endl;
+        std::cout << e.what();
+    }
 
-        std::cout << tree << std::endl << std::endl;
+    std::cout << std::endl;
 
-        
-        try
-        {
-            TBinaryTree::TNode * found = tree.Find(2);
-            if(found)
-                std::cout << found->Data;
-        }
-        catch (const TNotFoundException & e)
-        {
-            std::cout << "Exception works" << std::endl;
-            std::cout << e.what();
-        }
-
-                
-        std::cout << std::endl;
-
-        
-        try
-        {
-            TBinaryTree::TNode * found_ = tree.Find(15);
-            if(found_)
-                std::cout << found_->Data;
-        }
-        catch (const TNotFoundException & e)
-        {
-            std::cout << "Exception works" << std::endl;
-            std::cout << e.what();
-        }
-        
+    try
+    {
+        TBinaryTree::TNode * found_ = tree.Find(15);
+        if(found_)
+            std::cout << found_->Data;
+    }
+    catch (const TNotFoundException & e)
+    {
+        std::cout << "Exception works" << std::endl;
+        std::cout << e.what();
     }
 }
